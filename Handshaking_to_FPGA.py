ec3bc2e28974e7575c2ff63ad6aec1018e2bea36
DPickle17 <90335735+DPickle17@users.noreply.github.com>
2021-11-23 17:49:54 -0500

    Update Handshaking_to_FPGA.py

--- Handshaking_to_FPGA.py
+++ Handshaking_to_FPGA.py
@@ -5,45 +5,46 @@ import RPi.GPIO as GPIO
 from time import sleep
 from time import process_time
 
+GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 
-# Set variable names for GPIO pin numbers
+#Set variable names for GPIO pin numbers
 data_pins = [7, 11, 13, 15, 12, 16, 18, 22]
 RTS_pin = 29
 RTR_pin = 32
 
 #Local variable
-CURRENT_RTS = 0      
+CURRENT_RTS = 0
 
-# Set up pins as input or output
-GPIO.setwarnings(False)
+#Set up pins as input or output
 GPIO.setup(data_pins, GPIO.OUT)
 GPIO.setup(RTS_pin, GPIO.OUT)
 GPIO.setup(RTR_pin, GPIO.IN)
 
-
-# IN A WHILE LOOP
+#In a while loop
 #updating the data pins
 def send_byte(new_data):
-  GPIO.ouput(data_pins,new_data)
-  GPIO.input(data_pins)
+    GPIO.output(data_pins, new_data)
+    GPIO.input(data_pins)
 
 #assert RTS
-GPIO.output(RTS_pin, 1)   
+GPIO.output(RTS_pin, 1)
+print("RTS asserted")
 
 #wait until RTR is asserted
 while(1):
-  if(GPIO.input(RTR_pin) == 1)
-    print("RTR asserted")
-    break
+    if(GPIO.input(RTR_pin) == 1):
+        print("RTR asserted")
+        break
+#    print("RTR asserted")
+#    break
 
 #deassert RTS
 GPIO.output(RTS_pin, 0)
 print("RTS deasserted")
 
-#wait until RTR is deasserted
-while(1):
-  if(GPIO.input(RTR_pin) == 0)
-    print("RTR deasserted")
-    break
-
+#wait until RTR is deassserted
+while(1):     #was set equal to 1 but wouldnt get deasserted unless set to 0
+    if(GPIO.input(RTR_pin) == 0):
+        print("RTR deasserted")
+        break
